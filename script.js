80847f87611707022774e76d4590760524ec15a5
rebisha <rebisha67@gmail.com>
2019-07-10 22:29:12 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// Variables and data types
+var firstName = "Rebisha";
+console.log(firstName);
+
+var lastName = "Pokharel";
+var age = 23;
+console.log(lastName);
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+job = 'Teacher';
+console.log(job);
+
+// Variable mutation and type coercion
+var firstName = 'Rebisha';
+var age = 24;
+console.log(firstName + ' ' + age); //type coercion
+
+//variable mutation
+age = 'twenty four';
+job = 'Developer';
+
+alert(age + ' ' + job);
+lastName = prompt('last name?');
+console.log(lastName);
