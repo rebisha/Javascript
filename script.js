c70faa3a467c432e681459bca385354cda7c7a59
Rebisha Pokharel <rebisha.pokharel@isobar.com>
2019-08-06 17:47:12 +1000

    code challenge 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* // Variables and data types
+// Variables and data types
 var firstName = "Rebisha";
 console.log(firstName);
 
@@ -23,21 +23,17 @@ console.log(firstName + ' ' + age); //type coercion
 age = 'twenty four';
 job = 'Developer';
 
-alert(age + ' ' + job);
-lastName = prompt('last name?');
-console.log(lastName);
-
 //Mark and John BMI JavaScript
-var markMass = prompt("Mark, please enter your body mass.");
-var markHeight = prompt("Mark, please enter your height.");
-var markBmi = markMass / (markHeight ^ 2);
+/* var markMass = prompt("Mark, please enter your body mass in kg.");
+var markHeight = prompt("Mark, please enter your height in cm.");
+var markBmi = alert("Mark, your BMI is: " + (markMass / (markHeight * markHeight))*10000);
 
-var johnMass = prompt("John, please enter your body mass.");
-var johnHeight = prompt("John, please enter your height.");
-var johnBmi = johnMass / (johnHeight ^ 2);
+var johnMass = prompt("John, please enter your body mass in kg.");
+var johnHeight = prompt("John, please enter your height in cm.");
+var johnBmi = alert("John, your BMI is: " + (johnMass / (johnHeight * johnHeight))*10000);
 
 var isMarkHigherBmi = markBmi > johnBmi;
-console.log(isMarkHigherBmi); */
+console.log(isMarkHigherBmi);  */
 
 
 function calcAverage(num1, num2, num3) {
@@ -63,3 +59,45 @@ if(johnTeamAvg > mikeTeamAvg && johnTeamAvg > maryTeamAvg) {
 } else {
     console.log(`Mike's team are the winner.`);
 }
+
+var bills = [124, 48, 268];
+var tips = [];
+
+function tipCalculator(bills) {
+    bills.forEach(bill => {
+        if(bill < 50) {
+            tips[0] = bill * 0.2;
+        } else if(bill > 200) {
+            tips[1] = bill * 0.1;
+        } else {
+            tips[2] = bill * 0.15;
+        }
+    });  
+}
+tipCalculator(bills);
+console.log(tips);
+
+var johnObj = {
+    name : 'John Smith',
+    mass : 64,
+    height : 164,
+    bmi : calcBMI(this.mass, this.height)
+}; 
+
+var markObj = {
+    name: 'Mark Young',
+    mass: 78,           
+    height: 70,
+    bmi : calcBMI(this.mass, this.height)
+};
+
+function calcBMI(mass, height) {
+    return mass/(height*height);
+}
+
+if(johnObj.bmi > markObj.bmi) {
+    console.log('John has higher BMI of ' + johnObj.bmi);
+} else {
+    console.log('Mark has higher BMI of ' + markObj.bmi);
+}
+
