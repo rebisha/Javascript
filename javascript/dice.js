b0b18248ef14a158063c1b5c0c6df5b7e88a3342
Rebisha Pokharel <rebisha.pokharel@isobar.com>
2019-10-15 16:52:58 +1100

    m

--- javascript/dice.js
+++ javascript/dice.js
@@ -3,7 +3,7 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
@@ -13,9 +13,13 @@ var activePlayer = 0;
 
 var dice = Math.floor(Math.random() * 6) + 1;
 
-//selecting the element whic has current id and concatinating the activePlayer value to get the exact selector i.e #current-0 or #current-1
+//selecting the element which has current id and concatenating the activePlayer value to get the exact selector i.e #current-0 or #current-1
 document.querySelector('#current-' + activePlayer).innerHTML = dice; 
 
-/*in order to pass in html markup in innerHtml write it as a string i.e element.innerHtml = '<div>' + roundscore + '</div>'*/
+/*in order to pass in html markup in innerHtml write it as a string i.e element.innerHtml = '<div>' + roundScore + '</div>'*/
 
-//document.querySelector('.dice').innerHTML = `<img src="../assets/images/dice-` + dice + `.png" alt="dice" >`;
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+   // do something 
+});
